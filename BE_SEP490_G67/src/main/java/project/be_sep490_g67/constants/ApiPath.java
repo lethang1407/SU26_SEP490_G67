ccfa6f18080348004709dfcc23bb747c863522e2
ThangLD <thangle84264@gmail.com>
2026-06-17 17:55:27 +0700

    update api view store

--- BE_SEP490_G67/src/main/java/project/be_sep490_g67/constants/ApiPath.java
+++ BE_SEP490_G67/src/main/java/project/be_sep490_g67/constants/ApiPath.java
@@ -4,4 +4,5 @@ public class ApiPath {
     public static final String BASE_URL_V1 = "/api";
     public static final String USER = BASE_URL_V1 + "/users";
     public static final String AUTH = BASE_URL_V1 + "/auth";
+    public static final String STORE = BASE_URL_V1 + "/store";
 }
--- BE_SEP490_G67/src/main/java/project/be_sep490_g67/controller/StoreController.java
+++ BE_SEP490_G67/src/main/java/project/be_sep490_g67/controller/StoreController.java
@@ -1,4 +1,30 @@
 package project.be_sep490_g67.controller;
 
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.be_sep490_g67.constants.ApiPath;
+import project.be_sep490_g67.dto.response.ApiResponse;
+import project.be_sep490_g67.dto.response.StoreResponse;
+import project.be_sep490_g67.service.StoreService;
+
+@RestController
+@RequestMapping(ApiPath.STORE)
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class StoreController {
+    StoreService storeService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    ApiResponse<StoreResponse> getStoreInfor(){
+        StoreResponse storeInfo = storeService.getStoreInfo();
+        return ApiResponse.<StoreResponse>builder()
+                .result(storeInfo)
+                .build();
+    }
 }
