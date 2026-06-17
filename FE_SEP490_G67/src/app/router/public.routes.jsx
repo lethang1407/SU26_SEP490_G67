f6171142fc885925938f37ca1e26f13ea2e4e1f7
ThangLD <thangle84264@gmail.com>
2026-06-17 18:13:07 +0700

    update fe: public router

--- FE_SEP490_G67/src/app/router/public.routes.jsx
+++ FE_SEP490_G67/src/app/router/public.routes.jsx
@@ -1,17 +1,11 @@
 import { Navigate } from "react-router-dom";
 import LoginPage from "../../features/auth/LoginPage";
-import PublicLayout from "../../components/layouts/PublicLayout";
 import NotFound from "../../components/ui/not-found/NotFound";
 
 const publicRoutes = [
-  {
-    element: <PublicLayout />,
-    children: [
-      { path: "/", element: <Navigate to="/login" replace /> },
-      { path: "/login", element: <LoginPage /> },
-      { path: "*", element: <NotFound /> },
-    ],
-  },
+  { path: "/", element: <Navigate to="/login" replace /> },
+  { path: "/login", element: <LoginPage /> },
+  { path: "*", element: <NotFound /> },
 ];
 
 export default publicRoutes;
