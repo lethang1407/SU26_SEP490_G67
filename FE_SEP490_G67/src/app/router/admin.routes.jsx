6b93395223af2e60452e615072ff1a2e0597f65a
bvd512004 <ducbach512k4@gmail.com>
2026-06-18 04:47:48 +0700

    Feature/Create Staff and Detail Staff

--- FE_SEP490_G67/src/app/router/admin.routes.jsx
+++ FE_SEP490_G67/src/app/router/admin.routes.jsx
@@ -1,12 +1,22 @@
 
 import AdminDashboard from '../../features/dashboard/pages/AdminDashboard';
 import StaffManagementPage from '../../features/staff/pages/StaffManagementPage';
+import AddStaffPage from '../../features/staff/pages/AddStaffPage';
+import StaffInfoPage from '../../features/staff/pages/StaffInfoPage';
 import StoreInfor from '../../features/store/pages/StoreInfor';
 
 const adminRoutes = [
     {
         path: '/admin/dashboard',
         element: <AdminDashboard />,
+    },
+    {
+        path: '/admin/staff/create',
+        element: <AddStaffPage />,
+    },
+    {
+        path: '/admin/staff/:staffId',
+        element: <StaffInfoPage />,
     },
     {
         path: '/admin/staff',
