3e4f49d19390bd97aa1e333b4d68e5aedd2ed7f4
huythai200 <thaiphamhuy3@gmail.com>
2026-06-24 23:00:08 +0700

    update API and router navigate to page POS

--- FE_SEP490_G67/src/app/router/admin.routes.jsx
+++ FE_SEP490_G67/src/app/router/admin.routes.jsx
@@ -6,6 +6,8 @@ import StaffInfoPage from '../../features/staff/pages/StaffInfoPage';
 import StoreInfor from '../../features/store/pages/StoreInfor';
 import SupplierListPage from '../../features/supplier/pages/SupplierListPage';
 import SupplierDetailPage from '../../features/supplier/pages/SupplierDetailPage';
+import NotificationSettingsPage from '../../features/settings/pages/NotificationSettingsPage';
+import POSScreen from '../../features/pos-screen/pages/POS';
 
 const adminRoutes = [
     {
@@ -39,6 +41,14 @@ const adminRoutes = [
     {
         path: '/admin/warehouse/supplier/:id',
         element: <SupplierDetailPage />,
+    },
+    {
+        path: '/admin/settings/notifications',
+        element: <NotificationSettingsPage />,
+    },
+    {
+        path: '/admin/pos',
+        element: <POSScreen />,
     },
 ];
 
