d85010c069de1d0bc49cb15127d2a8cb85d0a505
ThangLD <thangle84264@gmail.com>
2026-06-25 03:10:06 +0700

    create api create, update category

--- BE_SEP490_G67/src/main/java/project/be_sep490_g67/service/CategoryService.java
+++ BE_SEP490_G67/src/main/java/project/be_sep490_g67/service/CategoryService.java
@@ -8,12 +8,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.be_sep490_g67.dto.request.CategoryRequest;
 import project.be_sep490_g67.dto.response.CategoryResponse;
 import project.be_sep490_g67.dto.response.PageResponse;
 import project.be_sep490_g67.entity.Category;
+import project.be_sep490_g67.exception.AppException;
+import project.be_sep490_g67.exception.ErrorCode;
 import project.be_sep490_g67.mapper.CategoryMapper;
 import project.be_sep490_g67.repository.CategoryRepository;
-import project.be_sep490_g67.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
@@ -22,7 +24,6 @@ import project.be_sep490_g67.repository.UserRepository;
 public class CategoryService {
 
     CategoryRepository categoryRepository;
-    UserRepository userRepository;
     CategoryMapper categoryMapper;
 
     // View all category
@@ -49,5 +50,46 @@ public class CategoryService {
                 .totalElements(result.getTotalElements())
                 .totalPages(result.getTotalPages())
                 .build();
+    }
+
+    // Create new category
+    @Transactional
+    public CategoryResponse createCategory(CategoryRequest request){
+        Category category = new Category();
+        category.setName(request.getName().trim());
+        category.setDescription(request.getDescription());
+
+        Category newCategory = categoryRepository.save(category);
+        log.info("Created category with id {}", category.getId());
+        return categoryMapper.toCategoryResponse(newCategory);
+    }
+
+    // Update category
+    @Transactional
+    public CategoryResponse updateCategory(
+            CategoryRequest request,
+            Integer categoryId) {
+
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() ->
+                        new AppException(ErrorCode.NOT_FOUND_CATEGORY));
+
+        String name = request.getName().trim();
+
+        if (categoryRepository.existsByNameIgnoreCaseAndIdNot(name, categoryId)) {
+            throw new AppException(ErrorCode.CATEGORY_ALREADY_EXISTS);
+        }
+
+        category.setName(name);
+        category.setDescription(
+                request.getDescription() == null
+                        ? null
+                        : request.getDescription()
+        );
+
+        categoryRepository.save(category);
+        log.info("Updated category with id {}", categoryId);
+
+        return categoryMapper.toCategoryResponse(category);
     }
 }
