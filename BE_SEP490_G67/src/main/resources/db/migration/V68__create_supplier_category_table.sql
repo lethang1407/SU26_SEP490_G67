ddcf3eec0f6015ba0a9621e8427e1e46eff5fbe9
ThangLD <thangle84264@gmail.com>
2026-09-21 15:00:09 +0700

    fix: thay utf8mb4_0900_ai_ci bang utf8mb4_unicode_ci

--- BE_SEP490_G67/src/main/resources/db/migration/V68__create_supplier_category_table.sql
+++ BE_SEP490_G67/src/main/resources/db/migration/V68__create_supplier_category_table.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS `supplier_category` (
   KEY `FK_supplier_category_category` (`category_id`),
   CONSTRAINT `FK_supplier_category_supplier` FOREIGN KEY (`supplier_id`) REFERENCES `suppliers` (`id`) ON DELETE CASCADE,
   CONSTRAINT `FK_supplier_category_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ;
