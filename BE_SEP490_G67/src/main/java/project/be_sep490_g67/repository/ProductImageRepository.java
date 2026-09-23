bcb8546ed28b29ef0d20f718053a2bbc4299a09c
huythai2004 <thaiphamhuy3@gmail.com>
2026-09-24 01:08:21 +0700

    update thêm ảnh sản phẩm vào POS, chỉnh sửa lỗi duplicate notification

--- BE_SEP490_G67/src/main/java/project/be_sep490_g67/repository/ProductImageRepository.java
+++ BE_SEP490_G67/src/main/java/project/be_sep490_g67/repository/ProductImageRepository.java
@@ -4,12 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.be_sep490_g67.entity.ProductImage;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
     List<ProductImage> findByProductIdAndIsRemovedFalseOrderBySortOrderAscIdAsc(Integer productId);
+
+    List<ProductImage> findByProductIdInAndIsRemovedFalseOrderBySortOrderAscIdAsc(Collection<Integer> productIds);
 
     Optional<ProductImage> findByIdAndProductIdAndIsRemovedFalse(Integer id, Integer productId);
 
